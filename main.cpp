6480d00b142cf1cc7024e290ba85ae4ae64547cf
Heider Sanchez <heider.sanchez.pe@gmail.com>
2020-09-24 08:06:41 -0500

    fix main

--- main.cpp
+++ main.cpp
@@ -2,10 +2,14 @@
 
 int main() {
   DoublyList* list = new DoublyList();
-  insert(list, 2);
+  SortedInsert(list, 2);
   display(list); // 2
-  insert(list, 5);
+  SortedInsert(list, 5);
   display(list); // 2, 5
-  insert(list, 1);
+  SortedInsert(list, 1);
   display(list); // 1, 2, 5  
+  SortedInsert(list, 10);
+  display(list); // 1, 2, 5, 10  
+  SortedInsert(list, 4);
+  display(list); // 1, 2, 4, 5, 10  
 }
